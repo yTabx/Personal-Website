51f72aafcc3b68f99fd515a59fad36bad77a3f9d
Nicholas Iyamabo <niyamabo@gmail.com>
2021-07-29 20:17:31 +0100

    navbar styling and links changed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,7 +32,7 @@ function Navbar(){
                     <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
                         <li className = 'nav-item'>
                             <Link to='/' className ='nav-links' onClick = {closeMobileMenu}>
-                                Home
+                                About
                             </Link>
                         </li>
                         <li className = 'nav-item'>
