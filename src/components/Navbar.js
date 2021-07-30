16c850442bcbad3032a19763d0f11760dcd05797
Nicholas Iyamabo <niyamabo@gmail.com>
2021-07-30 13:37:26 +0100

    navbar onclick functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,6 +16,16 @@ function Navbar(){
             setButton(true)
         }
     };
+    const scrollToPosition = (height)  =>{   
+        console.log('scrolling to position '+height); 
+        var positionX = 0,         
+            positionY = height;    
+    
+             
+        window.scrollTo(positionX, positionY);    
+
+        
+    }
 
     //have the sign up button not show up every time we refresh
     useEffect(() => {
@@ -31,17 +41,17 @@ function Navbar(){
                    
                     <ul className = {click ? 'nav-menu active' : 'nav-menu'}>
                         <li className = 'nav-item'>
-                            <Link to='/' className ='nav-links' onClick = {closeMobileMenu}>
+                            <Link to='/' className ='nav-links' onClick = {() => scrollToPosition(50)}>
                                 About
                             </Link>
                         </li>
                         <li className = 'nav-item'>
-                            <Link to='/services' className ='nav-links' onClick = {closeMobileMenu}>
+                            <Link to='/' className ='nav-links' onClick = {() => scrollToPosition(0)}>
                                 <img className='logo-image' src = 'images/logo-circle.png' alt = 'logo' />
                             </Link>
                         </li>
                         <li className = 'nav-item'>
-                            <Link to='/products' className ='nav-links' onClick = {closeMobileMenu}>
+                            <Link to='/' className ='nav-links' onClick = {() => scrollToPosition(750)}>
                                 Portfolio
                             </Link>
                         </li>
