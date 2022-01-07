6dd6a7c81478c9ab274df98ab0fd061fa2c095b1
Nicholas I <nxi094@student.bham.ac.uk>
2022-01-07 18:52:43 +0000

    test

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -42,6 +42,20 @@ function HeroSection() {
                                 .start();
                         }}
                     />
+                    <Typewriter
+                        onInit={(typewriter) => {
+                            typewriter.typeString('Hello World!')
+                                .callFunction(() => {
+                                    console.log('String typed out!');
+                                })
+                                .pauseFor(2500)
+                                .deleteAll()
+                                .callFunction(() => {
+                                    console.log('All strings were deleted');
+                                })
+                                .start();
+                        }}
+                    />
                     Motivated by growth and innovation,
                 </p> 
                 <p>
