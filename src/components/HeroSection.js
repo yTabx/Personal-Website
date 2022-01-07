62f244e6743fe05db8536a2e15d3cc6bee76cca6
Nicholas I <nxi094@student.bham.ac.uk>
2022-01-07 20:46:53 +0000

    fixed br html tag

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -30,7 +30,7 @@ function HeroSection() {
 
                     <Typewriter
                         onInit={(typewriter) => {
-                            typewriter.typeString("<div>Var developer  = <\br> name: 'Nicholas Iyamabo', <\br> type: 'Student', location = 'University of Birmingham' <\br> properties: ['Web Development',<br> 'Software Development']").start();
+                            typewriter.typeString("<div>Var developer  = <br> name: 'Nicholas Iyamabo', <\br> type: 'Student', location = 'University of Birmingham' <br> properties: ['Web Development',<br> 'Software Development']").start();
                             
                         }}
                         
