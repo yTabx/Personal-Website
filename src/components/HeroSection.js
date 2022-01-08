bad7ab10a481fd920029ac186675b1517a24da96
Nicholas I <nxi094@student.bham.ac.uk>
2022-01-08 17:43:06 +0000

    blocked off typerwriter component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,22 +12,9 @@ import './HeroSection.scss';
 
 function HeroSection() {
     return (
-        
-        <div className = 'about-card' id = 'about'>
-            <pre id="typewriter">
-                <span classname ="var-highlight">Var</span> 
-                developer = 
-                    name: <span class="string-highlight">'Nicholas Iyamabo'</span>,
-                    type: <span class="string-highlight">'Student'</span>,
-                    location: <span class="string-highlight">'University of Birmingham'</span>,
-                    properties:[<span class="string-highlight">'Javascript'</span>,
-                                <span class="string-highlight">'HTML'</span>,
-                                <span class="string-highlight">'CSS'</span>];
-                 
-            </pre>
-            <div className = 'About-Text'>
-                <p>
-
+        <>
+            <div className='type-writer-block'>
+                <div className='type-writer'>
                     <Typewriter options={{
                         delay: 10,
                         skipAddStyles: false
@@ -35,32 +22,59 @@ function HeroSection() {
                         onInit={(typewriter) => {
                             typewriter.typeString("<pre> <span, style = 'color: peachpuff;'>Var</span> <span style = 'color: black;'> developer =</span><br>     <span style = 'color: black;'> name: </span><span style = 'color: rgba(253, 149, 90, 0.8);'> 'Nicholas Iyamabo'</span><span style = 'color: black;'>,</span><br>     <span style = 'color: black;'> type: </span><span style = 'color: rgba(253, 149, 90, 0.8);'> 'Student'</span><span style = 'color: black;'>,</span><br>     <span style = 'color: black;'> location: </span><span style = 'color: rgba(253, 149, 90, 0.8);'> 'University of Birmingham'</span><span style = 'color: black;'>,</span><br>     <span style = 'color: black;'> properties: [</span><span style = 'color: rgba(253, 149, 90, 0.8);'> 'Web Development'</span><span style = 'color: black;'>,</span><br>                         <span style = 'color: rgba(253, 149, 90, 0.8);'> 'Software Development'</span><span style = 'color: black;'>]</span></pre>").start();
                         }}
-                        
+
                     />
-                    
-                    Motivated by growth and innovation,
-                </p> 
-                <p>
-                    I aim to pursue a career within the tech industry. 
-                
-                    My name is Nicholas Iyamabo and I am a second-year Computer Science student at the University of Birmingham. 
-                    Feel free to explore this website and contact me on any of the socials below.
-                        
-                </p>
-            </div>
-            {/*
-            <div className = 'image-container'>
-                <div className= 'image-wrapper'>
-                    <ul className='image-trio'>
-                    <ImageCard src = 'images/UoB-clock.jpg' alt = 'image' href = 'https://commons.wikimedia.org/wiki/File:The_University_of_Birmingham_Clocktower_-_geograph.org.uk_-_444013.jpg' />
-                    <ImageCard src = 'images/img-8.jpg' alt = 'image' href =''/>
-                    <ImageCard src = 'images/img-9.png' alt = 'image' href = ''/>
-                        
-                    </ul>
                 </div>
             </div>
-            */}
-        </div>
+            <div className = 'about-card' id = 'about'>
+                <pre id="typewriter">
+                    <span classname ="var-highlight">Var</span> 
+                    developer = 
+                        name: <span class="string-highlight">'Nicholas Iyamabo'</span>,
+                        type: <span class="string-highlight">'Student'</span>,
+                        location: <span class="string-highlight">'University of Birmingham'</span>,
+                        properties:[<span class="string-highlight">'Javascript'</span>,
+                                    <span class="string-highlight">'HTML'</span>,
+                                    <span class="string-highlight">'CSS'</span>];
+                 
+                </pre>
+                <div className = 'About-Text'>
+                    <p>
+
+                        <Typewriter options={{
+                            delay: 10,
+                            skipAddStyles: false
+                        }}
+                            onInit={(typewriter) => {
+                                typewriter.typeString("<pre> <span, style = 'color: peachpuff;'>Var</span> <span style = 'color: black;'> developer =</span><br>     <span style = 'color: black;'> name: </span><span style = 'color: rgba(253, 149, 90, 0.8);'> 'Nicholas Iyamabo'</span><span style = 'color: black;'>,</span><br>     <span style = 'color: black;'> type: </span><span style = 'color: rgba(253, 149, 90, 0.8);'> 'Student'</span><span style = 'color: black;'>,</span><br>     <span style = 'color: black;'> location: </span><span style = 'color: rgba(253, 149, 90, 0.8);'> 'University of Birmingham'</span><span style = 'color: black;'>,</span><br>     <span style = 'color: black;'> properties: [</span><span style = 'color: rgba(253, 149, 90, 0.8);'> 'Web Development'</span><span style = 'color: black;'>,</span><br>                         <span style = 'color: rgba(253, 149, 90, 0.8);'> 'Software Development'</span><span style = 'color: black;'>]</span></pre>").start();
+                            }}
+                        
+                        />
+                    
+                        Motivated by growth and innovation,
+                    </p> 
+                    <p>
+                        I aim to pursue a career within the tech industry. 
+                
+                        My name is Nicholas Iyamabo and I am a second-year Computer Science student at the University of Birmingham. 
+                        Feel free to explore this website and contact me on any of the socials below.
+                        
+                    </p>
+                </div>
+                {/*
+                <div className = 'image-container'>
+                    <div className= 'image-wrapper'>
+                        <ul className='image-trio'>
+                        <ImageCard src = 'images/UoB-clock.jpg' alt = 'image' href = 'https://commons.wikimedia.org/wiki/File:The_University_of_Birmingham_Clocktower_-_geograph.org.uk_-_444013.jpg' />
+                        <ImageCard src = 'images/img-8.jpg' alt = 'image' href =''/>
+                        <ImageCard src = 'images/img-9.png' alt = 'image' href = ''/>
+                        
+                        </ul>
+                    </div>
+                </div>
+                */}
+            </div>
+        </>
     );
 }
 export default HeroSection
